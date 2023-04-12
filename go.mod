24797ca40363f939674326d0de28d4215bee56e2
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2023-04-12 11:53:59 -0400

    Add config, but warg can't handle dicts from yaml.
    See yamldictproblem branch. I'm gonna take a break from dealing
    with this for now.

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/jedib0t/go-pretty/v6 v6.4.6
 	github.com/miekg/dns v1.1.52
+	github.com/sourcegraph/conc v0.3.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 )
@@ -23,11 +24,13 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/xhit/go-str2duration/v2 v2.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/tools v0.3.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"go.bbkane.com/warg"
 	"go.bbkane.com/warg/command"
+	"go.bbkane.com/warg/config/yamlreader"
 	"go.bbkane.com/warg/flag"
 	"go.bbkane.com/warg/section"
 	"go.bbkane.com/warg/value/dict"
@@ -15,7 +16,7 @@ import (
 
 var version string
 
-func digOneCommand() command.Command {
+func digCmd() command.Command {
 	return command.New(
 		"Query DNS and count results",
 		runDig,
@@ -23,17 +24,18 @@ func digOneCommand() command.Command {
 			"--count",
 			"Number of times to dig",
 			scalar.Int(
-				scalar.Default(1),
+				scalar.Default(10),
 			),
+			flag.ConfigPath("dig.count"),
 			flag.Required(),
 		),
 		command.Flag(
 			"--fqdn",
 			"FQDNs to dig",
-			slice.String(
-				slice.Default([]string{"linkedin.com"}),
-			),
+			slice.String(),
+			flag.ConfigPath("dig.fqdns"),
 			flag.Required(),
+			flag.Alias("-f"),
 		),
 		command.Flag(
 			"--rtype",
@@ -42,30 +44,35 @@ func digOneCommand() command.Command {
 				slice.Default([]string{"A"}),
 				slice.Choices("A", "AAAA", "CNAME"),
 			),
+			flag.ConfigPath("dig.rtypes"),
 			flag.Required(),
+			flag.Alias("-r"),
 		),
 		command.Flag(
 			"--ns",
 			"Nameserver IP + port to query. Example: 198.51.45.9:53",
-			slice.String(
-				slice.Default([]string{"198.51.45.9:53"}),
-			),
+			slice.String(),
+			flag.ConfigPath("dig.nameservers"),
 			flag.Required(),
 		),
 		command.Flag(
 			"--ns-map",
 			"Map of name to nameserver IP. Can then use names as arguments to --ns",
 			dict.AddrPort(),
+			flag.ConfigPath("dig.nameserver-map"),
 		),
 		command.Flag(
 			"--subnet",
 			"Optional client subnet. 101.251.8.0 for China for example",
 			slice.String(),
+			flag.ConfigPath("dig.subnets"),
+			flag.Alias("-s"),
 		),
 		command.Flag(
 			"--subnet-map",
 			"Map of name to subnet. Can then use names as arguments to --subnet",
 			dict.Addr(),
+			flag.ConfigPath("dig.subnet-map"),
 		),
 		command.Flag(
 			"--timeout",
@@ -74,6 +81,7 @@ func digOneCommand() command.Command {
 				scalar.Default(2*time.Second),
 			),
 			flag.Required(),
+			flag.ConfigPath("dig.timeout"),
 		),
 	)
 }
@@ -85,11 +93,19 @@ func buildApp() warg.App {
 			"Query DNS and count results",
 			section.ExistingCommand(
 				"dig",
-				digOneCommand(),
+				digCmd(),
 			),
 		),
 		warg.AddColorFlag(),
 		warg.AddVersionCommand(version),
+		warg.ConfigFlag(
+			"--config",
+			[]scalar.ScalarOpt[string]{
+				scalar.Default("~/.config/shovel.yaml"),
+			},
+			yamlreader.New,
+			"Config",
+		),
 	)
 	return app
 }
